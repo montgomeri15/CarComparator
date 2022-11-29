fd32db3fdd7be9baa0e9e8ccac62a54cd3babf19
montgomeri15 <montgomeri15@gmail.com>
2022-11-29 12:22:49 +0200

    With comparator and tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,43 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    void testCarsSortedByYear(){
+        Car toyota = new Car(1990,2.4);
+        Car mazda = new Car(2006,1.8);
+        Car opel = new Car(1989,2.5);
+        Car acura = new Car(2019,1.6);
+
+        List<Car> carsList = Arrays.asList(toyota, mazda, opel, acura);
+        Collections.sort(carsList);
+
+        assertThat(carsList).isNotEmpty()
+                .contains(toyota, mazda, opel, acura)
+                .contains(opel, Index.atIndex(0))
+                .contains(toyota, Index.atIndex(1))
+                .contains(mazda, Index.atIndex(2))
+                .contains(acura, Index.atIndex(3));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    void testCarsSortedByEngine(){
+        Car toyota = new Car(1990,2.4);
+        Car mazda = new Car(2006,1.8);
+        Car opel = new Car(1989,2.5);
+        Car acura = new Car(2019,1.6);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        List<Car> carsList = Arrays.asList(toyota, mazda, opel, acura);
+        Set<Car> carsByEngineSortedSet = new TreeSet<>(Comparator.comparing(Car::getEngineCapacity));
+        carsByEngineSortedSet.addAll(carsList);
+
+        assertThat(carsByEngineSortedSet).isNotEmpty();
     }
 }
